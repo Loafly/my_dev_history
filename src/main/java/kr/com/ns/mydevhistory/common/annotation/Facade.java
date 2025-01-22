5cf9bf4a3af1142e11bb4ea9196bb848e17c1ee4
Loafly <38175978+Loafly@users.noreply.github.com>
2025-01-22 14:19:25 +0900

    feat : facade 어노테이션 추가

---
+++ src/main/java/kr/com/ns/mydevhistory/common/annotation/Facade.java
@@ -0,0 +1,12 @@
+package kr.com.ns.mydevhistory.common.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Facade {
+}
