77e256683247513993712ea2b544609646ec7706
Loafly <38175978+Loafly@users.noreply.github.com>
2025-01-22 14:18:54 +0900

    init

---
+++ src/main/java/kr/com/ns/mydevhistory/MyDevHistoryApplication.java
@@ -0,0 +1,13 @@
+package kr.com.ns.mydevhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDevHistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/com/ns/mydevhistory/MyDevHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.com.ns.mydevhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
